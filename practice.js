341c1a773bb6c14ea69942c002fdf43e05e4d540
Hack Reactor Student <student@hackreactor.com>
2017-06-20 11:03:19 -0500

    Practiced JS for the first time in the camp with prompts and vars and console logs.

--- practice.js
+++ practice.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // your code goes below
-
+  console.log("the page has loaded!");
+  var givenNumber = prompt("Give me a number!");
+  console.log("Your number is " + givenNumber);
+  if(givenNumber < 4){
+    console.log("This number is less than four")
+    }else{
+      console.log("This number is more than four!")
+  }
 })
