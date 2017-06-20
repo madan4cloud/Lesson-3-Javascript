a4d92822bf02b7ae5164f65f78a689d3b632b685
Hack Reactor Student <student@hackreactor.com>
2017-06-20 12:24:55 -0500

    Calculator with js

--- practice.js
+++ practice.js
@@ -1,4 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
   // your code goes below
 
+var answer = prompt ("What Would you like to caculate?");
+if (answer == "add") {
+var guess2 = Number(prompt('Give me the first number'));
+var guess1 = Number(prompt('Give me the second number'));
+alert (guess2 + guess1)
+} 
+
+else if (answer == "subtract") {
+var guess20 = Number(prompt('Give me the first number'));
+var guess10= Number(prompt('Give me the second number'));
+alert (guess20 - guess10)
+} 
+
+else if (answer == "divide") {
+var guess22 = Number(prompt('Give me the first number'));
+var guess11 = Number(prompt('Give me the second number'));
+alert (guess22 / guess11)
+} 
+
+else if (answer == "multiply") {
+var guess40 = Number(prompt('Give me the first number'));
+var guess30 = Number(prompt('Give me the second number'));
+alert (guess4 * guess3)
+}
 })
