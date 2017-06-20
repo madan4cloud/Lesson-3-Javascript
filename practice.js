f74646fb9f5a098d519b6bd0274440754e4673f2
Hack Reactor Student <student@hackreactor.com>
2017-06-20 11:02:53 -0500

    practiced javascript

--- practice.js
+++ practice.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   // your code goes below
-
-})
+  console.log("the page has loaded");
+  var givenNumber=prompt("Give me a number");
+  console.log(givenNumber);
+  if (givenNumber<4){
+    console.log("this number is less than four")
+  } else {
+    console.log("this number is NOT less than four!")
+  }
+}) 
